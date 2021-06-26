f0f16a62a5dae7f3bc981d4242a4f43ead098cb3
LuiDev2001 <luisqrja@gmail.com>
2021-06-26 10:11:25 -0500

    Clean project and create some components for start devolpment

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from "./assets/images/logo.png";
+import "./App.css";
+import "./assets/styles/reset.css";
+import "./assets/styles/grid.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} alt="logo" />
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header className="App-header">
+      <img src={logo} alt="logo" />
+    </header>
+  );
+};
+
+export default Header;
